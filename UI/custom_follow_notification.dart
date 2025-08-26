39505f1a32a45f7ea16447fca5eb5874c5ae9fde
Beybeyy <cortez2231644@mls.ceu.edu.ph>
2025-08-26 10:52:48 +0800

    1:15

---
+++ UI/custom_follow_notification.dart
@@ -0,0 +1,23 @@
+
+
+
+import 'dart:async';
+
+class CustomFollowNotification extends StatefulWidget {
+  const CustomFollowNotification({Key? key}) : super(key: key);
+
+  @override
+  State<CustomFollowNotification> createState() => _CustomFollowNotification
+}
+
+class _CustomFollowNotificationState extends State<CustomFollowNotification
+  @override 
+  Wideget build(BuildContext context) {
+    return Row(children: [
+     CircleAvatar(
+      radius: 25,
+      backgroundImage: AssetImage("").
+     )// CircleAvatar
+    ],); //Row
+  }    
+}
