634f7b5076675fa3a93d477a08d4106be81844a8
Beybeyy <cortez2231644@mls.ceu.edu.ph>
2025-08-28 21:00:05 +0800

    3:31

--- UI/custom_follow_notification.dart
+++ UI/custom_follow_notification.dart
@@ -11,13 +11,55 @@ class CustomFollowNotification extends StatefulWidget {
 }
 
 class _CustomFollowNotificationState extends State<CustomFollowNotification
+  bool follow = false;
   @override 
   Wideget build(BuildContext context) {
     return Row(children: [
      CircleAvatar(
       radius: 25,
       backgroundImage: AssetImage("").
-     )// CircleAvatar
-    ],); //Row
+
+     ),// CircleAvatar
+     const SizedBox(Width: 15),
+     Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "Dean Winchester",
+           style: Theme.of (context)
+              .textTheme
+              .headline3!
+              .copyWith(color: mainText),
+        ), // Text
+        const SizedBox(
+          height: 5, 
+        ), // SizedBox
+         Text(
+          "New following you . h1"
+           style: Theme.of (context)
+              .textTheme
+              .subtitle1!
+              .copyWith(color: mainText),
+        ), // Text
+      ],
+     ) //Column
+     Expanded(
+      child: Padding(
+        padding
+      )
+      child: CustomButton(
+        height: 40,
+        color: follow==false?imary:form,
+        textColor: follow==false? Color.white.mainText,
+        onTap: () {},
+          setState(() {
+            follow = follow;
+          }); 
+        text: "Follow"
+      ), //CustomButoon
+     ), // Expanded
+
+    ],
+   ); //Row
   }    
 }
